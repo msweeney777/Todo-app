ada3783014e7609f0649d5b9d7cf0bb9c261ec27
Matt Sweeney <msweeney777@gmail.com>
2019-03-08 22:51:02 -0500

    Added some functions to todo.js

--- todo.js
+++ todo.js
@@ -1,5 +1,21 @@
 const datastore = [];
 
+// Input functions:
 function add_task (task) {
   const id = datastore.length() + 1;
 }
+
+function edit_task(task) {
+
+}
+
+function delete_task(task) {
+
+}
+
+function complete_task(task){
+}
+
+// Output functions:
+
+function get_task ()
