727eff807af08cfed9d71d954910a1b18259ad07
Matt Sweeney <msweeney777@gmail.com>
2019-04-27 11:41:32 -0400

    Made major changes to several functions, and files; cleaned up todo.mjs

--- todo.mjs
+++ todo.mjs
@@ -1,25 +1,48 @@
 import fs from 'fs';
 import chalk from 'chalk';
+
+//Parse the command line for a command - targeting the pertinent function - a
+//task and a description
 const command = (process.argv.slice(2,3)).pop();
 const task = (process.argv.slice(3,4)).pop();
-const description = (process.argv.slice(4)).pop();
+const description = (process.argv.slice(4, 5)).pop();
+const update = (process.argv.slice(5, 6)).pop();
+const updateDes = (process.argv.slice(6, 7)).pop();
 
 // Read file and write state to disk:
+//
 //fs.readFile('/tmp/test', (err, data) => {
-//  if(err) {
-//    throw err;  
-//  }
+//  if(err) throw err;  
+//  
 //  let info = JSON.parse(data);
+//
 //  data = JSON.stringify(info, null, 2);
 //
 //  fs.writeFile('/tmp/test', data, (err) => {
-//    if(err) {
-//      return console.log(err);
-//    }
+//    if(err) throw err;    
 //  })
 //});
 
-// Command line input information: Task, description, due date:
+// For connecting parsed command/inputs with the pertinent function and output
+if(command == 'add') {
+  addTask(task, description);
+} else if(command == 'remove' && task == "--id" && !isNaN(description)) {
+  deleteTask(description);
+} else if(command == 'help') {
+  help();
+} else if(command == 'list') {
+  listTasks();
+} else if (command == 'edit') {
+  editTask(description);
+}  else if (command == 'update' && task == "--id" && !isNaN(description)) {
+  completeTask(description);
+} else if (command == 'completed') {
+  listCompleted();
+} else {
+  console.log('Error: Input not recognized ');
+}
+
+//In case there is nothing written into the JSON file:
 function setDefault () {
   let info = {todos: []};
   let data = JSON.stringify(info, null, 2);
@@ -29,26 +52,10 @@ function setDefault () {
     console.log('Default written to file');
   })
 }
-function handleResponse (input) {
-  const response = input.trim();
-  if(count == 0) {
-    process.stdout.write('\nDescription?\n');
-    newObj.Name = response;
-    console.log(newObj);
-  } else if (count == 1) {
-    newObj.Description = response; 
-    console.log(newObj);
-    dueDate();
-  } else if (count == 2){
-    newObj.Due_date = response; 
-    console.log(newObj);
-    add_task(newObj);
-    list_tasks();
-  }
-  count++; 
-}
-//setDefault();
+
 // Input functions:
+
+// Writes a task object to the datastore JSON file
 function addTask (task, description) {
   fs.readFile('datastore.json', (err, data) => {
     if(err) throw err;   
@@ -62,30 +69,42 @@ function addTask (task, description) {
       done: false
     });
     data = JSON.stringify(stat, null, 2);
-    console.log(data);
     fs.writeFile('datastore.json', data, (err) => {
-    if(err) throw err; 
-    console.log("Info written to file"); 
+      if(err) throw err; 
+      console.log("Task written to file"); 
+    listTasks();
     })
   })
-
 }
-function edit_task(task) {
-  for(let i = 0; i < datastore.length; i++){
-    if(task.Id === datastore[i].Id){
-      datastore[i].Name = task.Name;
-      datastore[i].Description = task.Description;
-      datastore[i].Due_date = task.Due_date;
+//Finds the task object with the matching id and edits both the task title and
+//task description
+function editTask(id) {
+  fs.readFile('datastore.json', (err, data) => {
+    if(err) throw err;   
+    let stat = JSON.parse(data);
+    
+    for (let key in stat.todos) {
+      if(stat.todos[key].id == id) {
+        stat.todos[key].task = update;
+        stat.todos[key].description = updateDes;
+      }
     }
-  }
-}
 
-function delete_task(id_) {
+    data = JSON.stringify(stat, null, 2);
+    fs.writeFile('datastore.json', data, (err) => {
+      if(err) throw err; 
+      console.log("Task edited"); 
+    listTasks();
+    })
+  })
+}
+//Targets the pertinent task object for removal from the datastore.json file
+function deleteTask(id) {
   fs.readFile('datastore.json', (err, data) => {
     if(err) throw err;   
     let stat = JSON.parse(data);
     if(stat.todos.length > 1) {
-      stat.todos = stat.todos.filter((el) =>  {return el.id != id_})
+      stat.todos = stat.todos.filter((el) =>  {return el.id != id})
     } else {
       stat.todos.pop(); 
     }
@@ -93,69 +112,77 @@ function delete_task(id_) {
     data = JSON.stringify(stat, null, 2);
     
     fs.writeFile('datastore.json', data, (err) => {
-    if(err) throw err; 
-    console.log("Task removed."); 
+      if(err) throw err; 
+      console.log("Task removed.");  
+    listTasks();
     })
   })
 }
 
-function complete_task(complete_task){
-  for(let i = 0; i < datastore.length; i++) {
-    if(complete_task.Id === datastore[i].Id){
-      if(!complete_task.isComplete){
-        datastore[i].isComplete = true;
+function completeTask(id){
+  fs.readFile('datastore.json', (err, data) => {
+    if(err) throw err;   
+    let stat = JSON.parse(data);
+
+    for(let el in stat.todos) {
+      if(stat.todos[el].id == id) {
+        stat.todos[el].done = true;
       }
     }
-  }
-}
+    let completed = [];
+    if(stat.todos.length > 1) {
+      completed = stat.todos.filter((el) =>  {return el.done == true})
+      stat.todos = stat.todos.filter((el) =>  {return el.done == false})
+    } else {
+      completed = stat.todos.pop(); 
+      stat.todos = stat.todos.pop();
+    }
+    //console.log(completed);
 
-if(command == 'add') {
-  addTask(task, description);
-} else if(command == 'remove') {
-  delete_task(description);
-} else if(command == 'delete-log') {
-  console.log('the delete-log command has not been wired in yet.')
-} else if(command == 'list') {
-  list_tasks();
+    data = JSON.stringify(stat, null, 2);
+    
+    fs.writeFile('datastore.json', data, (err) => {
+    if(err) throw err; 
+    console.log("Task updated."); 
+    listTasks();
+    })
+
+    data = JSON.stringify(completed, null, 2);
+    fs.writeFile('completedList.json', data, (err) => {
+    if(err) throw err; 
+    })
+  })
 }
 
 // Output functions:
 
-function get_task (get_task) {
-  for(let i = 0; i < datastore.length; i++){ 
-    if(get_task.Id === datastore[i].Id){
-      console.log(datastore[i]);
-      return datastore[i];
-    }
-    console.log('No such task exists.')
-  } 
-}
-
-function list_tasks () {
+// Shows a list of tasks stored in the datastore.json file on command
+function listTasks () { 
   fs.readFile('datastore.json', (err, data) => {
     if(err) throw err;   
-    let stat = JSON.parse(data);
-    const res = stat.todos;
-    res.forEach(({id, task, done, description}) => {
+    let stat = JSON.parse(data); 
+
+    stat.todos.forEach(({id, task, done, description}) => {
       console.log(`${chalk.magenta(id)} ${done ? chalk.green('DONE') : chalk.yellowBright('NOT DONE')} - ${chalk.whiteBright(task)} : \n${description}`);
-    })
+    })  
   })
 }
 
-function list_completed () {
-  for(let i = 0; i < datastore.length; i++){ 
-    if(datastore[i].isComplete){
-      completed_list.push(datastore[i]);
-      console.log(completed_list);
-    }
-  } 
+function listCompleted () {
+  fs.readFile('completedList.json', (err, data) => {
+    if(err) throw err;   
+    let stat = JSON.parse(data); 
+
+    stat.completed.forEach(({id, task, done, description}) => {
+      console.log(`${chalk.magenta(id)} ${done ? chalk.green('DONE') : chalk.yellowBright('NOT DONE')} - ${chalk.whiteBright(task)} : \n${description}`);
+    })  
+  })
 } 
 
-function list_deleted () {
-  console.log("Deleted Tasks:")
-  console.log(deleted_list);
+function listDeleted () {
+
 }
 
-function list_active () {
-
+function help () {
+  console.log(`Commands are as follows: \nadd [task] [task description] \nedit --id [id#] [task] [task description] \nupdate --id [id#] \nlist`)
 }
